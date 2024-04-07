6f14bc9661bc49d4e865c8fcbed73683a1d6652d
Igor <130799393+igorek-666@users.noreply.github.com>
2024-04-07 13:57:38 +0200

    zadanie1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.5, 2.5); //inicjujemy wspolrzedne
+        //wyswietlamy wspolrzedne
+        System.out.println("Współrzędne:");
+        System.out.println("x = " +point.getX()); 
+        System.out.println("y = " +point.getY());
+    } 
+}
