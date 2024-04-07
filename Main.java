ad7a8eae2b9c5571fcc770f052ad86520e6191a7
Igor <130799393+igorek-666@users.noreply.github.com>
2024-04-07 14:38:43 +0200

    zadanie3

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
-import geometry.Circle;
+import geometry.ColoredCircle;
 import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
         Point center = new Point(3.5, 2.5);
-        Circle circle = new Circle(center, 7.0);
+        ColoredCircle coloredCircle = new ColoredCircle(center, 7.0, "violet");
+
         // Wyświetlanie właściwości koła
-        System.out.println("Właściwości koła:");
-        System.out.println("Promień: " + circle.getRadius());
-        System.out.println("Środek: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Obwód: " + circle.calculatePerimeter());
-        System.out.println("Pole: " + circle.getArea());
+        System.out.println("Właściwości kolorowego koła:");
+        System.out.println("Promień: " + coloredCircle.getRadius());
+        System.out.println("Środek: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Obwód: " + coloredCircle.calculatePerimeter());
+        System.out.println("Pole: " + coloredCircle.getArea());
+        //nowa wlasciwiosc - kolor
+        System.out.println("Kolor: " + coloredCircle.getColor());
+
     }
 }
