17bb5c9635e43cc58fec6f64aa71ffad2e71d388
Igor <130799393+igorek-666@users.noreply.github.com>
2024-04-07 15:01:02 +0200

    zadanie5

--- Main.java
+++ Main.java
@@ -1,20 +1,12 @@
-import geometry.Point;
-import geometry.Circle;
-import geometry.ColoredCircle;
+import geometry.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-        Circle[] circles = new Circle[3];
-        circles[0] = new Circle(new Point(3.5, 2.5), 7.0);
-        circles[1] = new ColoredCircle(new Point(4.5, 6.0), 8.0, "violet");
-        circles[2] = new ColoredCircle(new Point(2.0, 3.5), 10, "pink");
+       Rectangle rectangle = new Rectangle(5.0, 12.0);
+       double pole = rectangle.calculateArea();
+       double obwod = rectangle.calculatePerimeter();
 
-        for (int i = 0; i < circles.length; i++) {
-            double area = circles[i].getArea();
-            System.out.println("Pole tego koła: " + area);
-            if (circles[i] instanceof ColoredCircle) {
-                String color = ((ColoredCircle) circles[i]).getColor();
-                System.out.println("Kolor koła: " + color);
-            }
-        }
+       System.out.println("Pole: " + pole);
+       System.out.println("Obwód: " + obwod);
     } 
 }
